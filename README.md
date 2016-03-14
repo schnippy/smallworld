88aa31f6aa036419120285fc21002932088b0eb2
Greg Schnippel <schnippy@gmail.com>
2016-03-13 21:01:18 -0400

    switching over to unobtrusive configuration, need to resolve javascript namespacing issues

---
+++ README.md
@@ -0,0 +1,7 @@
+/* 
+
+ Still under development and not working and not recommended for production environments.. 
+
+*/
+
+The Smallworld module is a Drupal 7 module that integrates the handy Smallworld.js jquery library. 
--- js/smallworld.drupal.js
+++ js/smallworld.drupal.js
@@ -2,7 +2,14 @@
 
   Drupal.behaviors.smallworld = {
     attach: function (context, settings) {
-
+      console.log(Smallworld);
+      $.getJSON('/sites/all/libraries/smallworld.js/dist/world.json', function (data) {
+        $('.map').smallworld({
+          geojson: data,
+        });
+      });
+      $('div.map').css("border","2px solid green");
+      alert(Drupal.settings.smallworld_drupal.smallworld_water);
     }
   };
 }(jQuery));
