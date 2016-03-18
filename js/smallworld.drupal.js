dfa5cb9b15b433565c4c3b19e1494932ef9a22ff
Greg Schnippel <schnippy@gmail.com>
2016-03-18 09:57:04 -0400

    rolling up working version, with solution to jquery precedence issue

--- js/smallworld.drupal.js
+++ js/smallworld.drupal.js
@@ -2,15 +2,65 @@
 
   Drupal.behaviors.smallworld = {
     attach: function (context, settings) {
-      var el = document.querySelector('.map');
-      var map = new Smallworld(el, options);
-      $.getJSON('/sites/all/libraries/smallworld.js/dist/world.json', function (data) {
-        $('.map').smallworld({
-          geojson: data,
+      (function (factory) {
+        'use strict';
+
+          if (typeof define === 'function' && define.amd) {
+            define(['jquery'], factory);
+          } else {
+            factory(window.jQuery || window.Zepto, window.Smallworld);
+          }
+
+      }(function ($, Smallworld) {
+
+      'use strict';
+
+      // --------------------------------------------------------------------------
+      // Register plugin with jQuery
+      // --------------------------------------------------------------------------
+
+      $.extend($.fn, {
+        smallworld: function (options) {
+          var opts = $.extend({}, $.fn.smallworld.defaults, options);
+          return this.each(function () {
+
+          $(this).data('smallworld', new Smallworld(this, opts));
+          return this;
+        });
+
+      }
+
+      });
+
+      // --------------------------------------------------------------------------
+      // Define default plugin options.
+      // --------------------------------------------------------------------------
+
+      $.fn.smallworld.defaults = {};
+
+      }));
+
+      var smallworld_geojson = {};
+      $.ajax({
+    	url: "/sites/all/libraries/smallworld.js/dist/world.json",
+    	async: false,
+    	dataType: 'json',
+    	success: function(data) {
+    		smallworld_geojson = data;
+    	}
+      });
+
+      $('.smallworld').each(function() {
+        var lattitude = $(this).attr("data-lat");
+        var longitude = $(this).attr("data-long");
+        alert(longitude); alert(lattitude);
+        $(this).smallworld({
+          geojson: smallworld_geojson,
+          marker: [lattitude, longitude],
         });
       });
-      $('div.map').css("border","2px solid green");
-      alert(Drupal.settings.smallworld_drupal.smallworld_water);
+ 
+
     }
   };
 }(jQuery));
